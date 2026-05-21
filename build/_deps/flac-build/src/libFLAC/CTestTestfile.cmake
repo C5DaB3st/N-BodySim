46e05feba29bc2673b2a73604a1703052f9a401c
C5DaB3st <ban.haidar81@gmail.com>
2026-05-21 02:26:52 -0500

    Removed build from tracking

--- build/_deps/flac-build/src/libFLAC/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/CsDaBest/Documents/Projects/N_Body_Sim/build/_deps/flac-src/src/libFLAC
-# Build directory: C:/Users/CsDaBest/Documents/Projects/N_Body_Sim/build/_deps/flac-build/src/libFLAC
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
